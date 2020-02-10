c88a3a2b26b7ecb3e54425c4f08b6d42258cc731
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-10 20:38:01 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,39 @@
+def square_array(array) 
+  counter = 0 
+  new_array = []
+  
+  while  counter < array.length do
+  new_array << array[counter] ** 2
+  counter += 1
+  end
+new_array
+  
+end
+
+
+# define method square_array
+# square each element in an array
+# returns new array with the squared numbers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # https://www.youtube.com/watch?v=5ZfLLv2unbU&feature=youtu.be
 
@@ -12,13 +48,13 @@
 #     new_array
 # end
 
-def square_array(array)
-  new_array = []
-  counter = 0 
+# def square_array(array)
+#   new_array = []
+#   counter = 0 
   
-  while array[counter] do
-    new_array << array[counter] **2
-    counter += 1 
-  end
-  new_array
-end
+#   while array[counter] do
+#     new_array << array[counter] **2
+#     counter += 1 
+#   end
+#   new_array
+# end
