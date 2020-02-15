ad182c731e0bf9bce47193cef6b83e058c1f0a21
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-15 22:03:09 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,3 @@
-# at some point you'll probably do something like "new_array = [(collection of new squared numbers)]"
-# bc the lab asks for a new array to be returned!!!!
-# use the WHILE iterator!!!
-# no .each .map .collect .inject
-
-
 def square_array(array)
   new_array = []
   counter = 0
@@ -11,9 +5,9 @@ def square_array(array)
   while counter < array.length do
     new_array << array[counter] **2
   counter += 1
- end
+end
  
- return new_array
+return new_array
 end
 
 ##biggest problem here was that you forgot about the shovel method you silly goose!!! go back & review it a few more times! :)
